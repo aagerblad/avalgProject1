1e8c43f2674e08555ccc77432e3d131203e4914f
Tobias Wikström <tob.wikstrom@gmail.com>
2014-11-12 18:10:14 +0100

    lite längre

--- quadS.cpp
+++ quadS.cpp
@@ -10,30 +10,48 @@
 #include <vector>
 #include <gmpxx.h>
 #include <mpfr.h>
-//#include "gmpxx.h"
 using namespace std;
+
+#define GET_BIT_AT(index) ((numbers[index>>3] & (1<<(index&7))) >> (index&7))
+#define SET_BIT_AT(index) (numbers[index>>3] |= (1 << (index&7)))
+#define CLEAR_BIT_AT(index) (numbers[index>>3] &= ~(1 << (index&7)))
 
 
 class quadS {
     
+    
 private:
     
-    vector<int> basePrimes;
+    typedef struct modular_root {
+        unsigned long root1;
+        unsigned long root2;
+    } modular_root_t;
+    
+    // Quadtratic sieve members
+    vector<int64_t> basePrimes;
+    vector<modular_root> modular_roots;
     mpz_t N;
     mpz_t rootN;
     mpz_t rootN2;
     mpz_t qValLastX;
     mpz_t lastValueForXOffset;
+    int64_t quadraticPrimesFound;
+    
+    // Eratosthenes members
+    char *numbers;
+    int64_t base_ref;
     
 public:
     
     
     quadS() {}
     
-    ~quadS() {}
+    ~quadS() {
+//        delete[] modular_roots;
+    }
     
-    quadS(int s_N) {
-        
+    quadS(mpz_t N) {
+        init(N);
     }
     
     
@@ -48,6 +66,7 @@ public:
         mpz_init(rootN);
         mpz_init(rootN2);
         mpz_init(lastValueForXOffset);
+        mpz_init(qValLastX);
         mpz_set(N, n);
         
         mpz_sqrt(rootN, n);
@@ -60,6 +79,8 @@ public:
         
         
     }
+    
+    
     
     /* 1. Generate factor base */
     
@@ -82,6 +103,117 @@ public:
         
     }
     
+    void generateModularRoots() {
+//        modular_roots = new modular_root[quadraticPrimesFound];
+        mpz_t tmp, r1, r2;
+        mpz_init(tmp);
+        mpz_init(r1);
+        mpz_init(r2);
+        modular_roots.resize(quadraticPrimesFound);
+        
+        for (int i = 0; i < quadraticPrimesFound; i++) {
+            mpz_set_ui(tmp, (unsigned long) basePrimes[i]);
+            mpz_sqrtrem(r1, N, tmp); /* calculate the modular root */
+            mpz_neg(r2, r1); /* -q mod n */
+            mpz_mod(r2, r2, tmp);
+
+            modular_roots[i].root1 = mpz_get_ui(r1);
+            modular_roots[i].root2 = mpz_get_ui(r2);
+        }
+        cout << modular_roots.size() << endl;
+        mpz_clear(tmp);
+        mpz_clear(r1);
+        mpz_clear(r2);
+    }
+    
+
+    /************************************************/
+    /*              Eratosthenes sieve              */
+    /************************************************/
+    
+    
+    //Can handle up to base 2^31 * 8 * 8
+    int64_t sieve_primes_up_to(int64_t base) {
+        int64_t num_primes = 0;
+        int64_t i;
+        
+        //Find primes, base included
+        base++;
+        
+        numbers = (char*) calloc(base / 64 + 1, sizeof(uint64_t));
+        base_ref = base;
+        
+        for (i = 0; i < base; i++)
+            SET_BIT_AT(i);
+        
+        int64_t p = 2;
+        num_primes++;
+        int64_t offset;
+        
+        while (1) {
+            offset = 2 * p;
+            
+            while (offset < base) {
+                CLEAR_BIT_AT(offset);
+                offset += p;
+            }
+            
+            offset = p + 1;
+            while (offset < base && (GET_BIT_AT(offset) == 0)) {
+                offset++;
+            }
+            
+            if (offset == base)
+                break;
+            
+            p = offset;
+            num_primes++;
+        }
+        
+        return num_primes;
+    }
+    
+    /* Array must be malloc'ed already
+     * fills the array with the first @num_primes primes calculated with sieve_primes
+     */
+    void fill_primes(int64_t *primes_array) {
+        int64_t j, i;
+        
+        for (j = 0, i = 2; i < base_ref; i++) {
+            if (GET_BIT_AT(i) == 1) {
+                primes_array[j] = i;
+                j++;
+            }
+        }
+        
+        free(numbers);
+    }
+    
+    /* Fill the array with only primes where n is a quadratic residue: x² = n (mod p) */
+    int64_t fill_primes_with_quadratic_residue(mpz_t n) {
+        int64_t j, i;
+        mpz_t b;
+        mpz_init(b);
+        
+        basePrimes.push_back(2);
+        for (j = 1, i = 3; i < base_ref; i++) {
+            mpz_set_ui(b, (unsigned long) i);
+            if ((GET_BIT_AT(i)) == 1 && mpz_jacobi(n, b) == 1) {
+//                basePrimes[j] = i;
+                basePrimes.push_back(i);
+                j++;
+            }
+        }
+        
+        free(numbers);
+        quadraticPrimesFound = j;
+        return j;
+    }
+    
+    /************************************************/
+    /*               Helper functions               */
+    /************************************************/
+    
     void printMembers() {
         char * s;
         s = mpz_get_str(NULL, 10, rootN);
@@ -99,7 +231,7 @@ public:
         char * s;
         s = mpz_get_str(NULL, 10, a);
         cout <<  s << endl;
-
+        
     }
     
     void gcd(mpz_t & ret, mpz_t a, mpz_t b) {
@@ -114,5 +246,23 @@ public:
 		}
 		mpz_set(ret, b);
 	}
+    
+    int64_t toInt(mpz_t v) {
+        return mpz_get_ui(v);
+    }
+    
+    vector<int64_t> getPrimes() {
+        return basePrimes;
+    }
+    
+    vector<modular_root> getModularRoots() {
+        return modular_roots;
+    }
+    
+    void printModularRoots() {
+        for (modular_root r : modular_roots) {
+            cout << r.root1 << " " << r.root2 << endl;;
+        }
+    }
     
 };
--- quadSTest.cpp
+++ quadSTest.cpp
@@ -22,56 +22,29 @@ int main(int argc, const char * argv[])
 {
     
     //    qs hej("1232");
-    quadS hej("81");
     mpz_t N, B;
     mpz_init(N);
     mpz_init(B);
-    mpz_set_str(N, "81", 10);
-    hej.generateFactorBase(B, N);
+    mpz_set_str(N, "87463", 10);
     
-    hej.print(B);
-    //    hej.test();
     
-    //    char * hej = "12312";
-    //    qs tjenare(hej);
+    quadS qs(N);
+    qs.generateFactorBase(B, N);
     
-    //    int n = 10000;
-    //	int x_fixed = 4711;
-    //	int cycle_size = 2;
-    //	int x = x_fixed;
-    //	int h = 1;
-    //
-    //	while (h == 1) {
-    //		int count = 1;
-    //
-    //		while (count <= cycle_size && h == 1) {
-    //			x = g(x, n);
-    //			count = count + 1;
-    //			h = gcd(x - x_fixed, n);
-    //		}
-    //
-    //		if (h != 1)
-    //			break;
-    //
-    //		cycle_size = 2 * cycle_size;
-    //		x_fixed = x;
-    //	}
-    //	cout << "\nThe factor is  " << h;
+    cout << "N: ";
+    qs.print(N);
+    cout << "Base: ";
+    qs.print(B);
     
-	pollardRho p(10403);
+    int64_t t = qs.sieve_primes_up_to((int64_t) qs.toInt(B));
+    cout << "Primes found: " << t << endl;
+    int64_t w = qs.fill_primes_with_quadratic_residue(N);
+    cout << "Quadtratic primes found: " <<  w << endl;
+    qs.generateModularRoots();
+    qs.printModularRoots();
     
-	// cout << p.calculateFactor() << endl;
     
-    //	std::vector<long long> v = p.getAllFactors();
-    //
-    //	for (auto it = v.begin(); it != v.end(); ++it) {
-    //		cout << *it << " ";
-    //	}
-    //	cout << endl;
     
-	// cout << p.gcd(4712, 1244) << endl;
-    
-	// cout << p.g(5) << endl;
     
     return 0;
 }
